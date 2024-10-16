eb0dd36ce5ab2d7d71db0dde8d8abec74272bf09
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-10-16 09:59:42 +0200

    Converter class added, changed rates fields to static

--- src/main/java/org/example/Rate.java
+++ src/main/java/org/example/Rate.java
@@ -1,9 +1,9 @@
 package org.example;
 
 public class Rate {
-    double sekToEuro = 0.08818581;
-    double sekToUsd = 0.0959208;
-    double euroToSek = 11.343854;
-    double usdToSek = 10.426805;
+    static double sekToEuro = 0.08818581;
+    static double sekToUsd = 0.0959208;
+    static double euroToSek = 11.343854;
+    static double usdToSek = 10.426805;
 
 }
